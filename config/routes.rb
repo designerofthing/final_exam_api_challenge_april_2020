7bce49424514c1426655d4001954a32b64bcb1ef
designerofthing <designerofthings@gmail.com>
2020-06-26 15:14:58 +0200

    creates, request spec, adds comment model, association and migrates

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'comments/create'
   namespace :api do
     resources :articles, only: [:index, :show]
+    resources :comments, only: [:create]
   end
 end
---
+++ spec/requests/comments_request_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'POST /api/comments', type: :requests do
+  let!(:article) {create(:article)}
+  
+    describe 'User can comment on an article'
+    before do 
+      post 'api/comments',
+      params: {
+        comment: {
+          body: 'I should have done more backend',
+          article_id: article.id
+        }
+      }  
+  end
+  
+  it 'returns a 200 response' do
+    expect(response).to have_http_status 200
+  end
+  
+  # it 'assigns comment to  article' do
+  #   expect(response_json['article']['id']).to eq article.id
+  # end
+  
+  end
+  
+  
